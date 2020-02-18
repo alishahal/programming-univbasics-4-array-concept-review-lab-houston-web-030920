f06fb181fb33d3df5a609f0ac518c06be7910d8a
Karim Macknojia <alishahal@msn.com>
2020-02-18 22:22:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,15 +15,21 @@ def find_max_value(array)
     array.length.times { |index| 
     if array[index]>max
        max = array[index]
-       
     end
     }
     max
+    
 end
 
 
 def find_min_value(array)
    
-    
+    min = array[0]
+    array.length.times { |index| 
+    if array[index]<min
+       min = array[index]
+    end
+    }
+    min 
      
 end
